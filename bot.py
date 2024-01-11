63aa358637d6df7b48a438e19f4eeac7a18b2a58
Aisha <ayeshashafique123@gmail.com>
2024-01-11 21:14:19 +0500

    fixed token issue/added extra message/added separate threads

--- bot.py
+++ bot.py
@@ -1,18 +1,13 @@
 from flask import Flask, request
 import requests
-import openai
 from openai import OpenAI
-import os
-from packaging import version
 from decouple import config
-import functions
 import time
 import json
 import pynamodb
 from pynamodb.models import Model
 import threading
 import queue
-
 
 OPENAI_API_KEY = config('OPENAI_API_KEY')
 WHATSAPP_TOKEN = config('WHATSAPP_PERMANENT_ACCESS_TOKEN')
@@ -25,31 +20,28 @@ class User_data(Model):
     class Meta:
         table_name = 'User_data_IML_DRP'
         region = 'us-east-1'
-        aws_access_key_id=AWS_ACCESS_KEY_ID
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-        
-    message_id = pynamodb.attributes.UnicodeAttribute(hash_key=True) ## hash key = True means this is the primary key
+        aws_access_key_id = AWS_ACCESS_KEY_ID
+        aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+    # hash key = True means this is the primary key
+    message_id = pynamodb.attributes.UnicodeAttribute(hash_key=True)
     incoming_message = pynamodb.attributes.UnicodeAttribute()
     reply = pynamodb.attributes.UnicodeAttribute()
+
 class User_threads(Model):
     class Meta:
         table_name = 'thread_data_IML_DRP'
         region = 'us-east-1'
-        aws_access_key_id=AWS_ACCESS_KEY_ID
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+        aws_access_key_id = AWS_ACCESS_KEY_ID
+        aws_secret_access_key = AWS_SECRET_ACCESS_KEY
     phone_number = pynamodb.attributes.UnicodeAttribute(hash_key=True)
     thread_ID = pynamodb.attributes.UnicodeAttribute()
-
-
 
 with open('assistant.json') as f:
     assistant_id = json.load(f)['assistant_id']
 
 # Create a Flask app
 app = Flask(__name__)
-
 client = OpenAI(api_key=OPENAI_API_KEY)
-
 print("Using Assistant:", assistant_id)
 
 def send_msg(msg, number):
@@ -70,44 +62,39 @@ def send_msg(msg, number):
     response = requests.post(
         'https://graph.facebook.com/v18.0/220890917766697/messages', headers=headers, json=json_data)
     print("\nResponse object\n", response.text)
-    
-    
+
 def handler():
-    # try:
     while True:
         res = res_q.get()
-        print("Got a request from the queue")
         send_msg("Please wait while I process your request.",
-                    res['entry'][0]['changes'][0]['value']['messages'][0]['from'])
-        
-        try: 
-            thread_id_client = User_threads.get(res['entry'][0]['changes'][0]['value']['messages'][0]['from']).thread_ID
-            print(f"Thread ID: {thread_id_client} retrieved for {res['entry'][0]['changes'][0]['value']['messages'][0]['from']}")
+                 res['entry'][0]['changes'][0]['value']['messages'][0]['from'])
+
+        try:
+            thread_id_client = User_threads.get(
+                res['entry'][0]['changes'][0]['value']['messages'][0]['from']).thread_ID
+            print(
+                f"Thread ID: {thread_id_client} retrieved for {res['entry'][0]['changes'][0]['value']['messages'][0]['from']}")
         except User_threads.DoesNotExist:
             thread_id_client = client.beta.threads.create().id
-            User_threads(phone_number=res['entry'][0]['changes'][0]['value']['messages'][0]['from'], thread_ID=thread_id_client).save()
+            User_threads(phone_number=res['entry'][0]['changes'][0]['value']
+                         ['messages'][0]['from'], thread_ID=thread_id_client).save()
             print(f"Thread ID: {thread_id_client} created for {res['entry'][0]['changes'][0]['value']['messages'][0]['from']}")
-            
-        
+
         user_input = res['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
-        
         client.beta.threads.messages.create(thread_id=thread_id_client,
                                             role="user",
                                             content=user_input)
         run = client.beta.threads.runs.create(thread_id=thread_id_client,
-                                                assistant_id=assistant_id)
+                                              assistant_id=assistant_id)
         print("Run started with ID:", run.id)
-        # print("Calling check_run_status")
         start_time = time.time()
-        # print("In run status check, with start time:", start_time)
         run_status = client.beta.threads.runs.retrieve(thread_id=thread_id_client,
-                                                        run_id=run.id)
-        
+                                                       run_id=run.id)
+
         while time.time() - start_time < 120:
             run_status = client.beta.threads.runs.retrieve(thread_id=thread_id_client,
-                                                            run_id=run.id)
+                                                           run_id=run.id)
             print("Checking run status:", run_status.status)
-
             if run_status.status == 'completed':
                 messages = client.beta.threads.messages.list(
                     thread_id=thread_id_client)
@@ -117,9 +104,8 @@ def handler():
                 for annotation in annotations:
                     message_content.value = message_content.value.replace(
                         annotation.text, '')
-                    
                 print("Run completed, returning response to ",
-                        res['entry'][0]['changes'][0]['value']['messages'][0]['from'])
+                      res['entry'][0]['changes'][0]['value']['messages'][0]['from'])
                 print("Response:", message_content.value)
                 send_msg(
                     message_content.value, res['entry'][0]['changes'][0]['value']['messages'][0]['from'])
@@ -128,10 +114,10 @@ def handler():
         if run_status.status != 'completed':
             print("Run timed out")
             send_msg("Sorry, I'm having trouble understanding you. Please try again.",
-                        res['entry'][0]['changes'][0]['value']['messages'][0]['from'])
-            
-        User_data(message_id=res['entry'][0]['changes'][0]['value']['messages'][0]['id'], incoming_message=user_input, reply=message_content.value).save()
-
+                     res['entry'][0]['changes'][0]['value']['messages'][0]['from'])
+        # add to database
+        User_data(message_id=res['entry'][0]['changes'][0]['value']['messages'][0]
+                  ['id'], incoming_message=user_input, reply=message_content.value).save()
 
 my_thread = threading.Thread(target=handler)
 my_thread.daemon = True
@@ -139,21 +125,22 @@ my_thread.start()
 
 @app.route('/webhooks', methods=['POST', 'GET'])
 def webhook():
-    print("\nRequest object\n",request)
+    print("\nRequest object\n", request)
     res = request.get_json()
     try:
-        if res['entry'][0]['changes'][0]['value']['messages'][0]['type'] == 'text' and res['entry'][0]['changes'][0]['value']['messages'][0]['id'] not in res_id_list and time.time() - int(res['entry'][0]['changes'][0]['value']['messages'][0]['timestamp'])  < 100:
+        if res['entry'][0]['changes'][0]['value']['messages'][0]['type'] == 'text' and res['entry'][0]['changes'][0]['value']['messages'][0]['id'] not in res_id_list and time.time() - int(res['entry'][0]['changes'][0]['value']['messages'][0]['timestamp']) < 100:
             print("\nCurrent time:", time.time())
-            print("\nMessage time:", int(res['entry'][0]['changes'][0]['value']['messages'][0]['timestamp']))
-            print("\nPayload\n",res)
-            res_id_list.append(res['entry'][0]['changes'][0]['value']['messages'][0]['id'])
+            print("\nMessage time:", int(
+                res['entry'][0]['changes'][0]['value']['messages'][0]['timestamp']))
+            print("\nPayload\n", res)
+            res_id_list.append(res['entry'][0]['changes']
+                               [0]['value']['messages'][0]['id'])
             res_q.put(res)
-            print("\nRes Queue\n",res_q)
-            # print the queue
-            for i in list(res_q.queue):
-                print(i)
-            print("\nRes id list\n",res_id_list)
-            
+            # print("\nRes Queue\n", res_q.queue)
+            # for i in list(res_q.queue):
+            #     print(i)
+            # print("\nRes id list\n", res_id_list)
+
     except KeyError:
         print("Key error")
         pass
